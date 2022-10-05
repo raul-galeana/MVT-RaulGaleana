4ce75713ba69c164eae02efa63a5e3425b879a54
Raul Galeana <raul_galeana@hotmail.com>
2022-10-05 12:46:16 -0500

    Carga 03 del proyecto - fin

--- MVTRaulGaleana/views.py
+++ MVTRaulGaleana/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime, timedelta
 from django.template import Context, Template, loader
@@ -10,7 +11,7 @@ def alta_familiar(request, apellidos, asignacion, invitados):
     sub_familia.save()
     
     template = loader.get_template('alta_familiar.html')
-    template_render = template.render({'sub_familia': sub_familia})
+    template_render = template.render({'sub-familia': sub_familia})
         
     return HttpResponse(template_render)
 
@@ -20,5 +21,5 @@ def fiesta_familia(request):
     
     template = loader.get_template('fiesta_familia.html')
     template_rend = template.render({'sub_familias': sub_familias})
-        
+    
     return HttpResponse(template_rend)
--- ver_familia/models.py
+++ ver_familia/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class Subfamilia(models.Model):
      apellidos = models.CharField(max_length=30)
      asignacion = models.CharField(max_length=50)
      invitados = models.IntegerField()
-     fecha_pago = models.DateField()
+     fecha_pago = models.DateField(null=True)
